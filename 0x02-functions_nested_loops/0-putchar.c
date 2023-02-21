c4935faf14aaccd6295a9512c0f24a33c61d3b2c
houssemchagtmi <48832000+houssemchagtmi@users.noreply.github.com>
2023-02-22 00:36:35 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
 #include "main.h"
-int main() {
-        _putchar("_");
-        _putchar("p");
-        _putchar("u");
-        _putchar("t");
-        _putchar("c");
-        _putchar("h");
-        _putchar("a");
-        _putchar("r");
-return (0);
+
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
+
 
