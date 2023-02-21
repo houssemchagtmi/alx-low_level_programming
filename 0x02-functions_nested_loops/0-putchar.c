c4f2bf0b97739846ef58a13411725fd6de3d4c6c
houssemchagtmi <houssemchagtmii@gmail.com>
2023-02-21 15:18:54 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,16 @@
-int main() {
-    char c = '_';
-    write(1, &c, 1);
-    c = 'p';
-    write(1, &c, 1);
-    c = 'u';
-    write(1, &c, 1);
-    c = 't';
-    write(1, &c, 1);
-    c = 'c';
-    write(1, &c, 1);
-    c = 'h';
-    write(1, &c, 1);
-    c = 'a';
-    write(1, &c, 1);
-    c = 'r';
-    write(1, &c, 1);
-    c = '\n';
-    write(1, &c, 1);
-    return 0;
+#include "main.h"
+
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,41 @@
 #ifndef main_h
 #define main_h
 
-int _putchar(char)
-void print_alphabet(void)
-void print_alphabet_x10(void)
-int _islower(int c)
-int _isalpha(int c)
-int print_sign(int n)
-int _abs(int)
-int print_last_digit(int)
-void jack_bauer(void)
-void times_table(void)
-int add(int, int)
-void print_to_98(int n)
-void print_times_table(int n)
-#endif
+/* putchar function */
+int _putchar(char c);
+
+/* function to print the alphabet in lowercase */
+void print_alphabet(void);
+
+/* function to print the alphabet in lowercase 10 times */
+void print_alphabet_x10(void);
+
+/* function that checks for lowercase character */
+int _islower(int c);
+
+/* function that checks for alphabetic character */
+int _isalpha(int c);
+
+/* function that prints the sign of a number */
+int print_sign(int n);
+
+/* function that computes the absolute value of an integer */
+int _abs(int);
+
+/* function that prints the last digit of a number */
+int print_last_digit(int);
+
+/* function that prints every minute of a day */
+void jack_bauer(void);
+
+/* function that prints the 9 times table, starting with 0 */
+void times_table(void);
+
+/* function that adds two integers and returns the result */
+int add(int, int);
+
+/* function that prints all natural numbers from n to 98, followed by a new line */
+void print_to_98(int n);
+
+#endif /* main_h */
+
