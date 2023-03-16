b42943f8aaf93c9c9b1f4c78b3a0e90d061628b5
houssemchagtmi <48832000+houssemchagtmi@users.noreply.github.com>
2023-03-16 23:16:41 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,46 +1,21 @@
 #include <stdlib.h>
-#include "main.h"
 
-/**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- *
- * Return: pointer to the memory area s
- */
-
-char *_memset(char *s, char b, unsigned int n)
+int *array_range(int min, int max)
 {
-	unsigned int i;
+    int *arr;
+    int i, len;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+    if (min > max)
+        return NULL;
 
-	return (s);
-}
+    len = max - min + 1;
 
-/**
- * _calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *ptr;
+    arr = malloc(sizeof(int) * len);
+    if (arr == NULL)
+        return NULL;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+    for (i = 0; i < len; i++)
+        arr[i] = min + i;
 
-	ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
+    return arr;
 }
