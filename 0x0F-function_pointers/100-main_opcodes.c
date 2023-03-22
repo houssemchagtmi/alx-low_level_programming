fa7ef9b9220767e39a825c05ad3769744d0c6012
houssemchagtmi <48832000+houssemchagtmi@users.noreply.github.com>
2023-03-22 11:40:08 +0000

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on incorrect argument count, 2 on negative bytes
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int bytes, i;
-	char *arr;
+    int num_bytes, i;
+    char *main_ptr = (char *)main;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
 
-	bytes = atoi(argv[1]);
+    num_bytes = atoi(argv[1]);
+    if (num_bytes < 0)
+    {
+        printf("Error\n");
+        return (2);
+    }
 
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	arr = (char *)main;
+    for (i = 0; i < num_bytes; i++)
+    {
+        printf("%02hhx", *(main_ptr + i));
+        if (i < num_bytes - 1)
+            printf(" ");
+    }
+    printf("\n");
 
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
-	}
-	return (0);
+    return (0);
 }
